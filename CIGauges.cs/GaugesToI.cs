c9af9d803bd44b2bf70f881410b32510bb5aac35
Mike Step <mikestep1@gmail.com>
2015-08-14 09:33:51 -0500

    first working

---
+++ CIGauges.cs/GaugesToI.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+
+namespace CIGauges.cs
+{
+    class GaugesToI
+    {
+        public static void write(DataSet ds, Program.Member usr, DataSet dserrors, OdbcConnection connIS)
+        {
+            Logger oLogger = new Logger();
+            Console.WriteLine("Writting Production");
+            String strWriteGauge = "insert into " + usr.idatalib + ".PDWD2PF" +   //enWS2PF
+                    " (" +  //14 fields
+                     "PDDTLC," +//last change date
+                     "AUXID," +
+                     "PDLEAS," +
+                     "PDRPYY," +
+                     "PDRPMM," +//5
+                     "PDRPDD," +//6
+                     "PDCUS," +
+                      "PDTBSW," +
+                      "WTRINO," + //7,2
+                      "PDRMKS," +  //10..50 chr
+                      "PDTN1," + //10
+                      "PDTG1," +// 7,3 gauge
+                      "PDTV1," +  // gross vol
+                      "BATT," + //13
+                      "BEAT," + //14
+                      "PDQTR," +//15 7,2
+                      "PDTUBP," +
+                      "PDCASP," +
+                      "PDCHOK," +
+                      "PDSPM" + //20
+                       " )" +
+               " values(" +  //must insert in order of fields
+                     "?," +
+                     "?," +
+                     "?," +
+                     "?," +
+                     "?, " +
+                     "?," +
+                     "?," +
+                     "?," +
+                     "?," +
+                     "?," +  //10
+                     "?," +
+                      "?," +
+                     "?," +
+                      "?," +
+                     "?," +
+                     "?," +
+                     "?," +
+                      "?," +
+                     "?," +
+                     "?" +//20
+                  ")";
+                DataTable DOGGDGauges = ds.Tables["DOGGGauges"];
+                //DataTable DOGGDownWellComp = ds.Tables["DOGGWellComp"];
+                // join the code to the adjustment codes, may have gas adj at some point
+                var GaugeDays = from T1 in DOGGDGauges.AsEnumerable()
+                                //join T2 in DOGGDownWellComp.AsEnumerable() on (int)T1["ID"] equals (int)T2["ID"]
+                                select new
+                                {
+                                    lease = T1.Field<string>("LeaseNumber").Trim(),
+                                    tank = T1.Field<string>("TankNumber").Trim(),
+                                    readingDecimal = T1.Field<Decimal>("DecimalReading"),//7,3
+                                    wasDOGG = T1.Field<Boolean>("WasDOGGEntered"),
+                                    readDate = T1.Field<DateTime>("ReadingDate"),
+                                    entryDate = T1.Field<DateTime>("OriginalEntryDate"),
+                                    changeDate = T1.Field<DateTime>("LastDateChanged"),
+                                    TubingPres = T1.Field<Int32>("TubingPressure"),
+                                    CasingPres = T1.Field<Int32>("CasingPressure"),
+                                    ChokeSiz = T1.Field<string>("ChokeSize"),
+                                   StrokesPer = T1.Field<double>("StrokesPerMinute"),
+                                    Remarks = T1.Field<string>("Remarks"),
+                                    TransferDirection = T1.Field<int>("TransferDirection")
+                                };
+                //must insert in order of fields
+                int growcount=0;
+                try
+                {
+                  foreach (var GRow in GaugeDays)
+                   {   //format each column of DOGG tickets for Iseries
+                    OdbcCommand writeGauge = new OdbcCommand(strWriteGauge, connIS);
+                    growcount++;
+                    try
+                    {
+                        int dateentry = DateIBM.MStoIBMDate(DateTime.Now);
+                        writeGauge.Parameters.AddWithValue("@PDDTLC",GRow.changeDate);//change date
+                        writeGauge.Parameters.AddWithValue("@AUXID",GRow.lease);
+                        writeGauge.Parameters.AddWithValue("@PDLEAS", GRow.lease);
+                        writeGauge.Parameters.AddWithValue("@PDRPYY", DateIBM.MStoIBMDateYY(GRow.readDate)); // GRow.readDate.Year);
+                        writeGauge.Parameters.AddWithValue("@PDRPMM", DateIBM.MStoIBMDateMM(GRow.readDate)); //GRow.readDate.Month);//5
+                        writeGauge.Parameters.AddWithValue("@PDRPDD", DateIBM.MStoIBMDateDD(GRow.readDate)); //GRow.readDate.Day);
+                        writeGauge.Parameters.AddWithValue("@PDCUS", "40");
+                        writeGauge.Parameters.AddWithValue("@PDTBSW", 0.00M);
+                        writeGauge.Parameters.AddWithValue("@WTRINO", 0.00M);
+                        writeGauge.Parameters.AddWithValue("@PDRMKS", GRow.Remarks); //10
+                        writeGauge.Parameters.AddWithValue("@PDTN1", GRow.tank);  // 15 digits
+                        writeGauge.Parameters.AddWithValue("@PDTG1", GRow.readingDecimal);//gauge  
+                        writeGauge.Parameters.AddWithValue("@PDTV1", 0.00M); // calc vol
+                        writeGauge.Parameters.AddWithValue("@BATT", "DOGG"); //
+                        writeGauge.Parameters.AddWithValue("@BEAT", " ");
+                        int ft = Convert.ToInt32(GRow.readingDecimal / (100));
+                        int ftinquarters = ft * 48;
+                        int inches = Convert.ToInt32(GRow.readingDecimal) - (ft * 100);
+                        int inchesinquarters = inches * 4;
+                        int ftandinches = (ft * 100) + inches;
+                        int quarters = Convert.ToInt32(((GRow.readingDecimal - ftandinches) * 100) / 25);
+                        int ttlqtrs = (ftinquarters + inchesinquarters + quarters);
+                        writeGauge.Parameters.AddWithValue("@PDQTR", ttlqtrs);
+                        writeGauge.Parameters.AddWithValue("@PDTUBP", GRow.TubingPres);
+                        writeGauge.Parameters.AddWithValue("@PDCASP", GRow.CasingPres);
+                        Int32 Choke = 0;  // convert choke size to 
+                        bool result = Int32.TryParse(GRow.ChokeSiz, out Choke);
+                        if (result)
+                            writeGauge.Parameters.AddWithValue("@PDCHOK", Choke);
+                        else
+                            writeGauge.Parameters.AddWithValue("@PDCHOK", 0);
+                        decimal Strokes = Convert.ToDecimal(GRow.StrokesPer); // GRow.StrokesPer
+                        decimal strokesper =0.0m; // Convert.ToDecimal((GRow.["StrokesPerMinute"].ToString()));
+                        strokesper = Math.Round(strokesper, 1, MidpointRounding.AwayFromZero); // Rounds "up"
+                        writeGauge.Parameters.AddWithValue("@PDSPM", 0.0m); 
+                        writeGauge.ExecuteNonQuery();
+                        oLogger.logErrorToLog("Gauge > Iseries:" + GRow.tank.ToString() + " Day:" + GRow.readDate.ToString() + " #:" + growcount.ToString(), usr.PGMNM, usr.logpath);
+                        //writeGauge.Dispose();
+                    }
+                    catch (Exception DBserror)
+                    {
+                        oLogger.logErrorToLog("Gauges write:" + DBserror.Message, usr.pgmname, usr.logpath) ;//
+                    }
+                    finally {  writeGauge.Dispose();}
+                }
+            }
+            catch (Exception DBserror)
+            {
+                oLogger.logErrorToLog("Gauges TO I" + DBserror.Message, usr.pgmname, usr.logpath);
+            }
+        }
+    }
+}
---
+++ CIGauges.cs/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Data;
+
+namespace CIGauges.cs
+{
+    class Program
+    {
+        public class Member  //iseries user
+        {
+            public string logpath  //path to store file
+            { get; set; }
+            public string datasource  //path 
+            { get; set; }
+            public string profile //user profile for iseries, entered
+            { get; set; }
+            public string password //user password for iseries, entered
+            { get; set; }
+            public string defaultcollection  //user password for iseries, entered
+            { get; set; }
+            public string LibraryList //user library list
+            { get; set; }
+            public string xmlpathname//user library list
+            { get; set; }
+            public string constr //user connection string
+            { get; set; }
+            public decimal pctcomplete //user connection string
+            { get; set; }
+            public string logname //set in pgm
+            { get; set; }
+            public string idatalib //set in pgm
+            { get; set; }
+            public string outtable//set in pgm
+            { get; set; }
+            public string sqlserver //set in pgm
+            { get; set; }
+            public string Ierrors //set in pgm
+            { get; set; }
+            public string daysback //set in pgm
+            { get; set; }
+            //public string ODBConnfile //set in pgm
+            //{ get; set; }
+            public string GaugeTable //set in pgm
+            { get; set; }
+            public string outdb //set in pgm
+            { get; set; }
+            public string outTable //set in pgm
+            { get; set; }
+            public string DOGGConnConfig //set in pgm
+            { get; set; }
+            public string ODBConn //set in pgm
+            { get; set; }
+            public string PGMNM //set in pgm
+            { get; set; }
+            public string ProdConn //set in pgm
+            { get; set; }
+            public string pgmname //set in pgm
+            { get; set; }
+            public string ilibfil //set in pgm
+            { get; set; }
+            public string delDOGG //set in pgm
+            { get; set; }
+            public Member()
+            {
+                logpath = ConfigurationManager.AppSettings["logpath"];
+                logname = ConfigurationManager.AppSettings["logname"];
+                password = ConfigurationManager.AppSettings["password"];
+                profile = ConfigurationManager.AppSettings["profile"];
+                datasource = ConfigurationManager.AppSettings["iseriesip"];
+                sqlserver = ConfigurationManager.AppSettings["sqlserver"];
+                idatalib = ConfigurationManager.AppSettings["idatalibrary"];
+                outtable = ConfigurationManager.AppSettings["outtable"];
+                Ierrors = ConfigurationManager.AppSettings["Iserieserrors"];
+                daysback = ConfigurationManager.AppSettings["daysback"];
+                GaugeTable = ConfigurationManager.AppSettings["GaugeTable"];
+                outdb = ConfigurationManager.AppSettings["outdb"];
+                outTable = ConfigurationManager.AppSettings["outTable"];
+                DOGGConnConfig = ConfigurationManager.ConnectionStrings["DOGGConn"].ToString();
+                ODBConn = ConfigurationManager.ConnectionStrings["ODBConn"].ToString();
+                ProdConn = ConfigurationManager.ConnectionStrings["ProductionConnection"].ToString();
+                PGMNM = ConfigurationManager.AppSettings["PGMNM"].ToString();
+                pgmname = ConfigurationManager.AppSettings["PGMNM"];
+                ilibfil = ConfigurationManager.AppSettings["ilibfil"];
+                delDOGG = ConfigurationManager.AppSettings["delDOGG"];
+            }
+        }
+        static void Main(string[] args)
+        {
+              Member usr = new Member();
+            OdbcConnection connIS = new OdbcConnection(usr.ODBConn);
+            DataSet ds = new DataSet();   
+            DataSet dserrors = new DataSet(); 
+            try
+            {
+                 IseriesErrors.TableBuild(dserrors); //.TableBuild(dserrors);
+                 Logger oLogger = new Logger();
+                 oLogger.createLogFile("Process Started, days look back=" + usr.daysback, usr.pgmname, usr.logpath );
+                 connIS.Open();
+                 IseriesErrors.AddRowM4(dserrors, "-", "-", DateTime.Today, "Starting:" + usr.pgmname, "1", usr.pgmname, usr.logpath, usr.ilibfil);
+                SqlConnection DOGGConn = new SqlConnection(usr.ProdConn );
+                DOGGConn.Open();
+                int daysback = Convert.ToInt32(usr.daysback);
+                TimeSpan daterange = new TimeSpan(daysback, 0, 0, 0, 0);  //days back to load  
+                DateTime dtBackDate = DateTime.Today.Subtract(daterange);
+                string stBackDate = dtBackDate.ToString("MM/dd/yy") ;
+                string strYYYYMM = (dtBackDate.Year * 100 + dtBackDate.Month).ToString();
+                DateTime dtfldBackDate = DateTime.Today.Subtract(daterange);  //date format must by '2013-01-01 00:00:00'
+                System.DateTime.Today.ToString("MM/dd/yy");
+                string strSelectDownTime = "select * from GaugeReadingsTransfer  DT  " +
+                     "  where ReadingDate >= '" + stBackDate + "' and TransferDirection = 1 order by leaseNumber, readingdate";
+                //string strSelectDownTime = "select * from GaugeReadingsTransfer  DT  " +
+                //"  where  TransferDirection = 1";
+                SqlDataAdapter adpDownTime = new SqlDataAdapter(strSelectDownTime, DOGGConn);
+                adpDownTime.Fill(ds, "DOGGGauges");
+                oLogger.logErrorToLog("Gauges Read from DOGG:" + ds.Tables["DOGGGauges"].Rows.Count.ToString(), usr.PGMNM, usr.logpath);
+                GaugesToI.write(ds, usr, dserrors, connIS);
+                //write dserrors to iseries
+                IseriesErrors.Write(dserrors, usr.ilibfil, connIS, usr.pgmname, usr.logpath);
+                if (usr.delDOGG == "yes")
+                {
+                    SqlCommand dltgauges = new SqlCommand("delete from GaugeReadingsTransfer where TransferDirection = 1", DOGGConn);
+                    dltgauges.ExecuteNonQuery();
+                    oLogger.logErrorToLog("D Gauges Deleted:" + ds.Tables["DOGGGauges"].Rows.Count.ToString(), usr.pgmname, usr.logpath);
+                }
+                DOGGConn.Close();
+                DOGGConn.Dispose();
+
+                connIS.Close();
+                connIS.Dispose();
+
+             }
+             catch (Exception DBserror)
+            {  Logger oLogger = new Logger();
+                oLogger.logErrorToLog("DBexcept" + DBserror.Message, usr.pgmname, usr.logpath);
+                IseriesErrors.Write(dserrors, usr.ilibfil, connIS, usr.pgmname, usr.logpath);
+            }
+        }
+        }
+    }
+
